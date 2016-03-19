51cbfe71b23c3d3ed06e233947143cf0ca96e57b
Max Sokolov <i@maxsokolov.net>
2016-03-19 17:01:12 +0300

    setup travis config

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 ![Tablet](https://raw.githubusercontent.com/maxsokolov/tablet/assets/tablet.png)
 
 <p align="left">
+<a href="https://travis-ci.org/maxsokolov/tablet"><img src="https://travis-ci.org/maxsokolov/tablet.svg" alt="Build Status" /></a>
 <a href="https://developer.apple.com/swift"><img src="https://img.shields.io/badge/Swift2-compatible-4BC51D.svg?style=flat" alt="Swift 2 compatible" /></a>
 <img src="https://img.shields.io/badge/platform-iOS-blue.svg?style=flat" alt="Platform iOS" />
 <a href="https://cocoapods.org/pods/tablet"><img src="https://img.shields.io/badge/pod-0.3.0-blue.svg" alt="CocoaPods compatible" /></a>
--- Tests/TabletTests.swift
+++ Tests/TabletTests.swift
@@ -1,10 +1,22 @@
 //
-//  TableDirectorTests.swift
-//  TabletDemo
+//    Copyright (c) 2015 Max Sokolov https://twitter.com/max_sokolov
 //
-//  Created by Max Sokolov on 19/03/16.
-//  Copyright © 2016 Tablet. All rights reserved.
+//    Permission is hereby granted, free of charge, to any person obtaining a copy of
+//    this software and associated documentation files (the "Software"), to deal in
+//    the Software without restriction, including without limitation the rights to
+//    use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+//    the Software, and to permit persons to whom the Software is furnished to do so,
+//    subject to the following conditions:
 //
+//    The above copyright notice and this permission notice shall be included in all
+//    copies or substantial portions of the Software.
+//
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+//    FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+//    COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+//    IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+//    CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 import XCTest
 import Tablet
@@ -49,7 +61,7 @@ class TabletTests: XCTestCase {
         let row = TableRowBuilder<String, UITableViewCell>(items: source)
             .action(.configure) { data -> Void in
 
-                XCTAssertNotNil(data.cell, "Action should have cell")
+                XCTAssertNotNil(data.cell, "Action should have a cell")
                 data.cell?.textLabel?.text = "\(data.item)"
             }
         
