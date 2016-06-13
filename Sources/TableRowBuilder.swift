6f3daa337b71e500189ca9499d3a9b0b78f5abdc
Max Sokolov <i@maxsokolov.net>
2016-06-13 13:51:02 +0300

    support Swift Package Manager

--- Sources/TableRowBuilder.swift
+++ Sources/TableRowBuilder.swift
@@ -18,6 +18,8 @@
 //    IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 //    CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+import UIKit
+
 public protocol RowBuilder {
 
     func rowItems() -> [Row]?
