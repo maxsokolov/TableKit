f39e7ac602e5dc079b2b945e5290e6408226bacb
bellebethcooper <bellebethcooper@gmail.com>
2019-09-07 15:49:07 +1000

    Updated Swift tools version and added missing requirements

--- Package.swift
+++ Package.swift
@@ -1,5 +1,20 @@
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
-    name: "TableKit"
+    name: "TableKit",
+
+    products: [
+        .library(
+            name: "TableKit",
+            targets: ["TableKit"]),
+    ],
+
+    targets: [
+        .target(
+            name: "TableKit",
+            path: "Sources")
+    ]
 )
